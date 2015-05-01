b10950d31248a74c475b86f651be1a34cbb8501e
Ziyi Bai <noisebai@gmail.com>
2015-04-30 23:19:26 -0400

    finish step-1: get geo data base on user id

--- find_geo.py
+++ find_geo.py
@@ -1,10 +1,22 @@
 import pymongo
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
 
 db = pymongo.MongoClient().trowser
+user_ids = []
 
 for tweet in db.twitter.find():
-    print tweet['user']['id_str']
-    print tweet['geo']
+    user_ids.append(tweet['user']['id_str'])
+    # print tweet['user']['id_str']
+    # print tweet['geo']
+unique_user_ids = set(user_ids)
 
-print "finish processing"
+#loop tweets base on user id
+#and store the geo data
+for id in unique_user_ids:
+    print id
+    geo_dict = {}
+    for tweet in db.twitter.find({'user.id_str':id}):
+        geo_dict.update(tweet['geo'])
+        print geo_dict
+# print type(unique_user_ids)
+print "Done"
